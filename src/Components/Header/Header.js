eaed90363fdc66575cf80dede9328f51f08610c5
Safiaahmed6043 <safiaahmed1786@gmail.com>
2021-09-26 17:39:49 +0600

    initial commit

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Header.css'
+
+const Header = () => {
+    return (
+        <div className="header">
+            <h3>Select Your Favorite Cartoon characters </h3>
+            <h1>Make <span> Own Team</span></h1>
+            <p>Let's Go back to the Childhood days!!</p>
+            <h2>Budget : 65 Million</h2>
+
+        </div>
+    );
+};
+
+export default Header;
---
+++ src/Components/Shop/Shop.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './Shop.css'
+
+const Shop = () => {
+    return (
+        <div>
+           <div className="product-container">
+
+              <h4>hkjlk;</h4>
+
+           </div>
+           <div className="cart-container">
+
+               <h4>Character Added : 0</h4>
+               <p>Total Cost : $0</p>
+               <button>Buy Now</button>
+           </div>
+        </div>
+    );
+};
+
+export default Shop;
